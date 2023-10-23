db233aa929cdf48e2f07341331a90b042e9c0462
Manju M <manju.m@coddletech.com>
2023-10-23 12:49:24 +0530

    updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,5 @@
 /* eslint-disable no-empty */
 
-// const mysql = require('mysql2');
-// const util = require('util');
-
-// const cont = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '1234',
-//   database: 'manju',
-// });
-// const query = util.promisify(cont.query).bind(cont);
-
-// module.exports = {
-//   cont,
-//   query,
-// };
-
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
